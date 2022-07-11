c2e5df6d04271a278af3b8f776dcff1ad732a4ce
oshay12 <108832686+oshay12@users.noreply.github.com>
2022-07-11 16:08:55 -0300

    Update and rename Main.java to test.java

--- test.java
+++ test.java
@@ -1,9 +1,6 @@
-public class Main {
+public class test {
     public static void main(String[] args) {
-        LotteryTicket lotto = new LotteryTicket(1001);
         LotteryDraw draw = new LotteryDraw(10);
-        System.out.println(lotto);
-
     }
 
     public static String padLeft(String s, int width)
